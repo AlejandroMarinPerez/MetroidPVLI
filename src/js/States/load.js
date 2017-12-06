0298ffec4ca0c65ef92b80ba474055451ba7bfe8
Diego Baratto <dbaratto@ucm.es>
2017-12-07 00:10:13 +0100

    done.exe

--- src/js/States/load.js
+++ src/js/States/load.js
@@ -8,7 +8,8 @@ var loadState = {
 		game.load.spritesheet('dude', 'assets/PlayerThings/letsTry2.png', 23, 64);
     	game.load.image('hand', 'assets/PowerUps/hand.png');
     	game.load.image('spike', 'assets/Enemies/spikes.png');
-    	game.load.image('bala', 'assets/PlayerThings/bala.png');
+    	game.load.spritesheet('bala', 'assets/PlayerThings/bala.png', 112, 176);
+    	game.load.spritesheet('rocket', 'assets/PlayerThings/rocket.png', 112, 176);
     	game.load.tilemap('mapaA', 'assets/Map/Brinstar4.json', null, Phaser.Tilemap.TILED_JSON); //el primer tile
     	game.load.image('gameTiles', 'assets/Map/TilesGrandes.png'); //las imagenes del tile
     	console.log("Cargando..."); //simple debug
